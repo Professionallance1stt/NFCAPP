e62b41baa422be8ecb96f2adb896475440476887
Professionallance1stt <professionallance1st@gmail.com>
2022-08-11 09:45:14 -0400

    Finished HandleContentURI, found a work around, making a complete version soon

--- app/src/main/java/com/codexpedia/nfcreadwrite/Rec.kt
+++ app/src/main/java/com/codexpedia/nfcreadwrite/Rec.kt
@@ -1,11 +1,60 @@
 package com.codexpedia.nfcreadwrite
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.TextUtils
+import java.io.File
 
 class Rec : AppCompatActivity() {
+    private var parentPath: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rec)
+    }
+
+    private fun handleContentUri(beamUri: Uri): File? =
+
+        if (beamUri.authority != MediaStore.AUTHORITY) {
+            val projection = arrayOf(MediaStore.MediaColumns.DATA)
+            val pathCursor = contentResolver.query(beamUri, projection, null, null, null)
+
+            if (pathCursor?.moveToFirst() == true) {
+                pathCursor.getColumnIndex(MediaStore.MediaColumns.DATA).let { filenameIndex ->
+                    pathCursor.getString(filenameIndex).let { fileName ->
+                        File(fileName)
+                    }.parentFile
+                }
+            } else {
+
+                null
+            }
+        } else {
+            null
+        }
+
+
+    fun handleFileUri(beamUri: Uri): File? =
+
+        beamUri.path.let { fileName ->
+            File(fileName)
+                .parentFile
+        }
+
+    private fun handleViewIntent() {
+
+        if (TextUtils.equals(intent.action, Intent.ACTION_VIEW)) {
+            // Get the URI from the Intent
+            intent.data?.also { beamUri ->
+                parentPath = when (beamUri.scheme) {
+                    "file" -> handleFileUri(beamUri)
+                    "content" -> handleContentUri(beamUri)
+                    else -> null
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/codexpedia/nfcreadwrite/Sending.kt
+++ app/src/main/java/com/codexpedia/nfcreadwrite/Sending.kt
@@ -1,11 +1,61 @@
 package com.codexpedia.nfcreadwrite
 
+import android.net.Uri
+import android.nfc.NfcAdapter
+import android.nfc.NfcEvent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.view.View
+import java.io.File
 
-class Sending : AppCompatActivity() {
+class Sending : AppCompatActivity()
+{
+    private lateinit var nfcAdapter: NfcAdapter
+    private var androidBeamAvailable = false
+    private val fileUris = mutableListOf<Uri>()
+
+    private inner class FileUriCallback : NfcAdapter.CreateBeamUrisCallback {
+
+        override fun createBeamUris(event: NfcEvent): Array<Uri> {
+            return fileUris.toTypedArray()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sending)
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this).apply {
+            val fileUris = mutableListOf<Uri>()
+            val transferFile = null
+            val extDir = getExternalFilesDir(null)
+            val requestFile = File(extDir, transferFile).apply {
+                setReadable(true, false)
+            }
+            Uri.fromFile(requestFile)?.also { fileUri ->
+                fileUris += fileUri
+            } ?: Log.e("My Activity", "No File URI available for file.")
+            FileUriCallback();
+            var fileUriCallback = FileUriCallback()
+            nfcAdapter.setBeamPushUrisCallback(fileUriCallback, this@Sending)
+
+
+        }
     }
+    fun sendFile(view: View?) {
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        val fileName = null
+        val fileDirectory = Environment
+            .getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES
+            )
+
+        val fileToTransfer = File(fileDirectory, fileName)
+        fileToTransfer.setReadable(true, false)
+
+    }
+
+
 }
